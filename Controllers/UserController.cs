fe3e22d75bb7117a1855f1029705289224e39790
Amir Syafiq <76594956+amirsyfiq@users.noreply.github.com>
2023-02-02 14:38:32 +0800

    Apply JWT

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace ShopAPI.Controllers
 {
@@ -8,24 +12,30 @@ namespace ShopAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly DataContext _context;
-        public UserController(DataContext context)
+        private readonly IConfiguration _configuration;
+
+        public UserController(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
 
         // USER REGISTER
         [HttpPost]
-        public async Task<IActionResult> Register(UserRegisterRequest request)
+        public async Task<ActionResult<User>> Register(UserRegisterRequest request)
         {
             if (_context.Users.Any(u => u.Email == request.Email))
                 return BadRequest("User is already exist!");
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
 
             var user = new User
             {
                 Name = request.Name,
                 Email = request.Email,
-                Password = request.Password
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
             };
 
             _context.Users.Add(user);
@@ -37,16 +47,60 @@ namespace ShopAPI.Controllers
 
         // USER LOGIN
         [HttpPost]
-        public async Task<IActionResult> Login(UserLoginRequest request)
+        public async Task<ActionResult<User>> Login(UserLoginRequest request)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
             if (user == null)
                 return BadRequest("User not found!");
 
-            if (request.Password != user.Password)
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
                 return BadRequest("Password is incorrect!");
+            }
+
+            string token = CreateToken(user);
 
             return Ok($"Welcome back {user.Name}!");
+            //return Ok(token);
+        }
+
+
+        // CREATE A PASSWORD HASH
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+
+        // CHECK & COMPARE IF THE USER PASSWORD IS CORRECT
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+
+        // CREATE & APPLY JSON WEB TOKEN (JWT)
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -5,7 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        //public string Password { get; set; } = string.Empty;
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+
 
         // RELATION WITH TABLE CART
         public List<Cart>? Carts { get; set; }
